dd692b850455f8ffe856eb9f84d3f39218e13825
Jayaprabahar Chandrasekaran <jpofficial@gmail.com>
2020-05-12 22:15:13 +0200

    Added time delay to call another microservice

--- bestmusicdb-artist-service/src/main/java/com/jayaprabahar/bestmusicdb/artist/consumer/AlbumServiceConsumer.java
+++ bestmusicdb-artist-service/src/main/java/com/jayaprabahar/bestmusicdb/artist/consumer/AlbumServiceConsumer.java
@@ -3,10 +3,19 @@
  */
 package com.jayaprabahar.bestmusicdb.artist.consumer;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 /**
  * <p> Project : com.jayaprabahar.bestmusicdb.controller </p>
@@ -26,7 +35,18 @@ public class AlbumServiceConsumer {
 	@Bean
 	@LoadBalanced
 	public WebClient.Builder loadBalancedWebClientBuilder() {
-		return WebClient.builder().baseUrl("http://album-service/albums/");
+		// Added delay which is expected
+		TcpClient tcpClient = TcpClient
+				  .create()
+				  .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+				  .doOnConnected(connection -> {
+				      connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+				      connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+				  });
+				 
+		return WebClient.builder()
+				.baseUrl("http://album-service/albums/")
+				.clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)));
 	}
 
 }
